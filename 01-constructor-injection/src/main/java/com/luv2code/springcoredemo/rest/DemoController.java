9cca8a550d1c335f53907c0e04d1e598ca1197d6
nkserveren26 <norinoritoshi@outlook.jp>
2024-03-07 22:56:42 +0900

    setter injection学習。

--- 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -8,11 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     // define a private field for the dependency
-    private final Coach myCoach;
+    private Coach myCoach;
 
     // define a constructor for dependency injection
+    /*
     @Autowired
     public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+     */
+
+    @Autowired
+    public void setCoach(Coach theCoach) {
         myCoach = theCoach;
     }
 
