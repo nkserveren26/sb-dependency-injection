6672ae844a992ff712c4e8644ab6e5e92fd10417
nkserveren26 <norinoritoshi@outlook.jp>
2024-03-08 23:59:25 +0900

    component scanning学習。

--- 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,18 +12,21 @@ public class DemoController {
     private Coach myCoach;
 
     // define a constructor for dependency injection
-    /*
+
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("trackCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
-     */
 
+
+    /*
     @Autowired
     public void setCoach(Coach theCoach) {
         myCoach = theCoach;
     }
+
+     */
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
