1c5f336506a5a573581f04050427b7676b9992ce
nkserveren26 <norinoritoshi@outlook.jp>
2024-03-09 22:30:54 +0900

    lazy annotation学習

--- 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -21,7 +21,8 @@ public class DemoController {
      */
 
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
 
