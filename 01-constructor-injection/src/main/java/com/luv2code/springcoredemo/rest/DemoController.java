83cdbe781a3f424b1b8c917ef9e53070e0dcae2a
nkserveren26 <norinoritoshi@outlook.jp>
2024-03-10 22:35:39 +0900

    Lifecycle method学習

--- 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -11,8 +11,6 @@ public class DemoController {
     // define a private field for the dependency
     private Coach myCoach;
 
-    private Coach anotherCoach;
-
     // define a constructor for dependency injection
 
     /*
@@ -24,11 +22,9 @@ public class DemoController {
 
     @Autowired
     public DemoController(
-            @Qualifier("cricketCoach") Coach theCoach,
-            @Qualifier("cricketCoach") Coach theAnotherCoach) {
+            @Qualifier("cricketCoach") Coach theCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
-        anotherCoach = theAnotherCoach;
     }
 
 
@@ -45,8 +41,10 @@ public class DemoController {
         return myCoach.getDailyWorkout();
     }
 
+    /*
     @GetMapping("/check")
     public String check() {
         return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
     }
+     */
 }
